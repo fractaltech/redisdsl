24054e78312aed886d4858c1b0e0101b0c8b15f0
Kapil Verma <kapv89@gmail.com>
2019-03-11 16:01:30 +0530

    redisdsl ready for production use

--- test/main.js
+++ test/main.js
@@ -1,80 +1,18 @@
 const tape = require('tape');
-const mock = require('mock-require');
-const sinon = require('sinon');
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
+const {client, get, set, del} = require('../src')();
 
-const log = (...args) => {
-  return new Promise((resolve, reject) => {
-    const str = util.format(...args);
+tape('testing redis cmds', async (t) => {
+  t.plan(2);
 
-    fs.writeFile(path.join(__dirname, 'log'), str, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(err);
-      }
-    });
-  });
-};
+  console.log('testing redis cmds');
 
-tape('redis cmds', (t) => {
-  t.plan(11);
+  await set('foo', 'bar');
+  const res1 = await get('foo');
+  t.ok(res1 === 'bar', 'testing set & get work');
 
-  const redisMock = {
-    createClient: () => ({
-      _cache: {},
-      get(key) {
-        return new Promise((resolve) => setTimeout(() => resolve(key in this._cache ? this._cache[key] : null), 9));
-      },
-      set(key, val) {
-        return new Promise((resolve) => setTimeout(() => {
-          this._cache[key] = val;
-          resolve();
-        }, 27));
-      },
-      del(key) {
-        return new Promise((resolve) => setTimeout(() => {
-          delete this._cache[key];
-          resolve();
-        }, 30));
-      }
-    })
-  };
+  await del('foo');
+  const res2 = await get('foo');
+  t.ok(res2 === null, 'testing del works');
 
-  t.ok(redisMock instanceof Object, 'fizz');
-
-  mock('redis', redisMock);
-
-  const {client, get, set, del} = require('../src')();
-
-  t.ok(client instanceof Object);
-  t.ok(get instanceof Function);
-
-  const getSpy = sinon.spy(client, 'get');
-  const setSpy = sinon.spy(client, 'set');
-  const delSpy = sinon.spy(client, 'del');
-
-  t.ok(getSpy instanceof Object);
-  t.ok(setSpy instanceof Object);
-  t.ok(delSpy instanceof Object);
-
-  set('foo', 'bar').then(() => {
-    return get('foo');
-  }).then((res) => {
-    t.ok(res === 'bar', 'set & get work');
-  }).then(() => {
-    return del('foo');
-  }).then(() => {
-    return get('foo');
-  }).then((res) => {
-    t.ok(res === null, 'del works');
-  }).then(() => {
-    t.ok(getSpy.calledTwice, 'getSpy');
-    t.ok(setSpy.calledOnce, 'setSpy');
-    t.ok(delSpy.calledOnce, 'delSpy');
-  }).then(() => {
-    mock.stop('redis');
-  });
+  await client.quit();
 });
